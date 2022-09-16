8fdf7980637d7e0c8f4e35955f0538af108ce283
Sameer Khan <sameerkhan5130@gmail.com>
2022-09-16 21:32:33 +0500

    unit test

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,6 +6,11 @@ class UserTest extends TestCase
     /**
      * /users [GET]
      */
+    
+    /**
+     * @covers App\Providers\AuthServiceProvider
+     * @covers App\Http\Controllers\UsersController::index
+     */
     public function testShouldReturnAllUsers(){
 
         $this->get("http://localhost:8000/api/users", []);
@@ -28,6 +33,11 @@ class UserTest extends TestCase
     /**
      * /user/id [GET]
      */
+
+    /**
+     * @covers App\Providers\AuthServiceProvider
+     * @covers App\Http\Controllers\UsersController::show
+     */
     public function testShouldReturnUser(){
         $this->get("http://localhost:8000/api/user/1", []);
         $this->seeStatusCode(200);
@@ -46,6 +56,13 @@ class UserTest extends TestCase
 
     /**
      * /user [POST]
+     */
+
+    /**
+     * @covers App\Providers\AuthServiceProvider
+     * @covers App\Http\Controllers\UsersController::create
+     * @covers App\Mail\WelcomeUser
+     * @covers App\Models\Users
      */
     public function testShouldCreateUser(){
 
@@ -74,6 +91,11 @@ class UserTest extends TestCase
 
     /**
      * /user/id [DELETE]
+     */
+
+     /**
+     * @covers App\Providers\AuthServiceProvider
+     * @covers App\Http\Controllers\UsersController::delete
      */
     public function testShouldDeleteUser(){
         
